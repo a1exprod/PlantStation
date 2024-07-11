d79774487f4cca792d675af778769db010b841a6
a1exprod <alexander.schnabl@outlook.com>
2024-07-11 11:20:09 +0200

    started designing

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{url('css/main-stylesheet.css')}}">
+    
+    <title>Dashboard</title>
+</head>
+<body>
+
+    <section id="css-dashboard">
+        <header>
+            <h1>Plantstation</h1>
+        </header>
+        <main>
+            <h2>Wheater</h2>
+        </main>
+        
+      
+    </section>
+
+    <section>
+        <h2>Plant Overview</h2>
+    </section>
+</body>
+</html>
